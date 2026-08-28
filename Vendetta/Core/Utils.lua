H0ssein/Vendetta-TBC-WOW-Addon

commit 8e86e8d1a060225a13d89467168d51fde41f342e

Fix: Prevent Account-Wide Data Leakage into Personal Data

Vendetta/Modules/DatabaseUI.lua
@@ -50,7 +50,7 @@ Ven.Popups["VENDETTA_CONFIRM_DELETE"] = {
 	button2 = "No",
 	OnShow = function(self, data)
 		local db = Ven.InitHeroDB()
-		local v = db and db[data]
+		local v = (db and db[data]) or Ven.playerCache[data] or Ven.SyncPlayerDataFromOtherHeroes(data)
 		if v then
 			local fIcon = Ven.GetFactionIcon(v.faction) or ""
 			local cIcon = Ven.GetClassIcon(v.classFile) or ""

Vendetta/Modules/Scanner.lua
@@ -347,31 +347,70 @@ combatLogFrame:SetScript("OnEvent", function(self, event, ...)
 			end
 			local rName, pName = GetRealmName() or "Unknown", UnitName("player") or "Unknown"
 			if VendettaDB[rName] then
+				Ven.AccountWanteds = {}
+				Ven.AccountBounties = {}
 				for hero, enemies in pairs(VendettaDB[rName]) do
-					if type(enemies) == "table" then
+					if type(enemies) == "table" and hero ~= pName and hero ~= "MyHeroes" then
 						for eName, eData in pairs(enemies) do
-							if type(eData) == "table" and (eData.isWanted or eData.isBounty) and hero ~= pName then
-								if not db[eName] then
-									db[eName] = {
-										kills = 0,
-										deaths = 0,
-										level = eData.level,
-										class = eData.class,
-										classFile = eData.classFile,
-										race = eData.race,
-										faction = eData.faction,
-										note = eData.note,
-										bountyNote = eData.bountyNote,
-										isWanted = eData.isWanted,
-										isBounty = eData.isBounty,
-									}
-								else
-									db[eName].isWanted = eData.isWanted
-									db[eName].isBounty = eData.isBounty
-									db[eName].bountyNote = eData.bountyNote
-								end
+							if type(eData) == "table" then
+								if eData.isWanted then Ven.AccountWanteds[eName] = true end
+								if eData.isBounty then Ven.AccountBounties[eName] = true end
 							end
 						end
+					end
+				end
+			end
+			-- Clean up ghost entries that were previously injected by the old syncing logic
+			local reservedKeys = {
+				showPersonalData = true,
+				showAccountWide = true,
+				showNetworkBounties = true,
+				filterFaction = true,
+				filterClass = true,
+				filterLevel70Only = true,
+				filterWantedOnly = true,
+				enableNetwork = true,
+				trackNetworkWanteds = true,
+				trackNetworkBounties = true,
+				shareWanteds = true,
+				shareBounties = true,
+				enableToasts = true,
+				toastDuration = true,
+				toastScale = true,
+				toastBgColor = true,
+				toastBorderColor = true,
+				toastAnchor = true,
+				toastX = true,
+				toastY = true,
+				ignoreBGKills = true,
+				ignoreArenaKills = true,
+				isTrackerHidden = true,
+				trackerScale = true,
+				trackerX = true,
+				trackerY = true,
+				trackerWidth = true,
+				bountyKillForceBG = true,
+				bountyKillSoundIdx = true,
+				bountySpotForceBG = true,
+				bountySpotSoundIdx = true,
+				wantedKillForceBG = true,
+				wantedKillSoundIdx = true,
+				wantedSpotForceBG = true,
+				wantedSpotSoundIdx = true,
+				trackAlliesInst = true,
+				trackAlliesWorld = true,
+				trackInSafeZones = true,
+				trackTargetsInst = true,
+				trackTargetsWorld = true,
+				trackWantedsInst = true,
+				trackWantedsWorld = true,
+			}
+			for eName, eData in pairs(db) do
+				if type(eData) == "table" and not reservedKeys[eName] then
+					if (eData.kills == 0 or eData.kills == nil) 
+					   and (eData.deaths == 0 or eData.deaths == nil) 
+					   and not eData.timeAdded then
+						db[eName] = nil
 					end
 				end
 			end
@@ -449,11 +488,13 @@ CreateFrame("Frame"):SetScript("OnUpdate", function(self, elapsed)
 					local isPersBounty = type(db[name]) == "table" and db[name].isBounty
 					local isNetBounty = db.enableNetwork and Ven.BountyBoard and Ven.BountyBoard[name]
 					local isNetWanted = db.enableNetwork and Ven.WantedBoard and Ven.WantedBoard[name]
+					local isAcctWanted = db.showAccountWide and Ven.AccountWanteds and Ven.AccountWanteds[name]
+					local isAcctBounty = db.showAccountWide and Ven.AccountBounties and Ven.AccountBounties[name]
 
 					local playedSpotSound = false
 
 					if scanWanteds and (not isSafeZone or db.trackInSafeZones) then
-						if isPersWanted or isPersBounty or isNetWanted then
+						if isPersWanted or isPersBounty or isNetWanted or isNetBounty or isAcctWanted or isAcctBounty then
 							local t = GetTime()
 							local timeSinceLastSeen = t - (Ven.wantedLastSeen[name] or 0)
 							Ven.wantedLastSeen[name] = t

commit 3d8aa18c374c0739d30b932afcbdd8f5c1598c93

Fix: Whitelist issue introduced in commit 8e86e8d

Vendetta/Core/Utils.lua
@@ -121,7 +121,21 @@ popup:SetFrameStrata("TOOLTIP")
 popup:SetBackdrop({ bgFile = "Interface\\Buttons\\WHITE8x8", edgeFile = "Interface\\Buttons\\WHITE8x8", edgeSize = 1 })
 popup:SetBackdropColor(0.05, 0.05, 0.05, 0.95)
 popup:SetBackdropBorderColor(0.3, 0.3, 0.3, 1)
+popup:EnableKeyboard(true)
+popup:SetPropagateKeyboardInput(true)
 popup:Hide()
+
+popup:SetScript("OnKeyDown", function(self, key)
+	if key == "ESCAPE" then
+		self:SetPropagateKeyboardInput(false)
+		if self.cfg and self.cfg.OnCancel then
+			self.cfg.OnCancel(self, self.data)
+		end
+		Ven.HidePopup()
+	else
+		self:SetPropagateKeyboardInput(true)
+	end
+end)
 popup.titleFrame = CreateFrame("Frame", nil, popup)
 popup.titleFrame:SetHeight(20)
 popup.titleFrame:SetPoint("TOP", 0, -20)
@@ -183,13 +197,13 @@ function Ven.SetPopupTitle(prefix, classFile, faction, nameStr, desc)
 end
 
 popup.editBox = CreateFrame("EditBox", nil, popup, "BackdropTemplate")
-popup.editBox:SetSize(200, 24)
+popup.editBox:SetSize(240, 24)
 popup.editBox:SetPoint("CENTER", 0, 5)
 popup.editBox:SetFontObject("GameFontHighlight")
 popup.editBox:SetBackdrop({ bgFile = "Interface\\Buttons\\WHITE8x8", edgeFile = "Interface\\Buttons\\WHITE8x8", edgeSize = 1 })
-popup.editBox:SetBackdropColor(0, 0, 0, 0.8)
-popup.editBox:SetBackdropBorderColor(0.3, 0.3, 0.3, 0.8)
-popup.editBox:SetTextInsets(6, 6, 0, 0)
+popup.editBox:SetBackdropColor(0.05, 0.05, 0.05, 0.9)
+popup.editBox:SetBackdropBorderColor(0.4, 0.4, 0.4, 1)
+popup.editBox:SetTextInsets(8, 8, 0, 0)
 popup.editBox:SetAutoFocus(false)
 
 popup.editBox.placeholder = popup.editBox:CreateFontString(nil, "OVERLAY", "GameFontDisable")
@@ -203,13 +217,17 @@ popup.editBox:SetScript("OnTextChanged", function(self)
 	end
 end)
 
+popup.editBox:SetScript("OnEscapePressed", function(self)
+	self:ClearFocus()
+end)
+
 popup.btn1 = CreateFrame("Button", nil, popup, "BackdropTemplate")
-popup.btn1:SetSize(100, 24)
-popup.btn1:SetPoint("BOTTOMLEFT", 60, 15)
+popup.btn1:SetSize(90, 24)
+popup.btn1:SetPoint("BOTTOMLEFT", 75, 15)
 Ven.StyleFlatButton(popup.btn1)
 popup.btn2 = CreateFrame("Button", nil, popup, "BackdropTemplate")
-popup.btn2:SetSize(100, 24)
-popup.btn2:SetPoint("BOTTOMRIGHT", -60, 15)
+popup.btn2:SetSize(90, 24)
+popup.btn2:SetPoint("BOTTOMRIGHT", -75, 15)
 Ven.StyleFlatButton(popup.btn2)
 
 function Ven.ShowPopup(cfg, data)
@@ -218,10 +236,14 @@ function Ven.ShowPopup(cfg, data)
 	popup.btn1:SetText(cfg.button1 or "Yes")
 	popup.btn2:SetText(cfg.button2 or "No")
 
+	if GameTooltip:IsShown() then
+		GameTooltip:Hide()
+	end
+
 	if cfg.hasEditBox then
-		popup:SetHeight(160)
+		popup:SetHeight(110)
 		popup.editBox:Show()
-		popup.editBox:SetPoint("CENTER", 0, -10)
+		popup.editBox:SetPoint("CENTER", 0, -5)
 		popup.editBox:SetText("")
 		if cfg.placeholder then
 			popup.editBox.placeholder:SetText(cfg.placeholder)
@@ -234,13 +256,13 @@ function Ven.ShowPopup(cfg, data)
 		else
 			popup.editBox:SetMaxLetters(255)
 		end
-		popup.btn1:SetPoint("BOTTOMLEFT", 60, 15)
-		popup.btn2:SetPoint("BOTTOMRIGHT", -60, 15)
+		popup.btn1:SetPoint("BOTTOMLEFT", 75, 15)
+		popup.btn2:SetPoint("BOTTOMRIGHT", -75, 15)
 	else
-		popup:SetHeight(130)
+		popup:SetHeight(90)
 		popup.editBox:Hide()
-		popup.btn1:SetPoint("BOTTOMLEFT", 60, 20)
-		popup.btn2:SetPoint("BOTTOMRIGHT", -60, 20)
+		popup.btn1:SetPoint("BOTTOMLEFT", 75, 15)
+		popup.btn2:SetPoint("BOTTOMRIGHT", -75, 15)
 	end
 	
 	if cfg.OnShow then
@@ -277,11 +299,11 @@ popup.btn2:SetScript("OnClick", function()
 	if popup.cfg and popup.cfg.OnCancel then
 		popup.cfg.OnCancel(popup, popup.data)
 	end
-	popup:Hide()
+	Ven.HidePopup()
 end)
-popup.editBox:SetScript("OnEscapePressed", function(self)
+function Ven.HidePopup()
 	popup:Hide()
-end)
+end
 popup.editBox:SetScript("OnEnterPressed", function(self)
 	popup.btn1:Click()
 end)

Vendetta/Modules/DatabaseUI.lua
@@ -254,6 +254,7 @@ Ven.Popups["VENDETTA_ADD_PLAYER"] = {
 	button1 = "Add",
 	button2 = "Cancel",
 	hasEditBox = true,
+	placeholder = "Enter Player Name",
 	OnAccept = function(self, data, inputStr)
 		if inputStr then
 			local name = inputStr
@@ -322,6 +323,8 @@ optBtn:SetScript("OnClick", function()
 	if Ven.OptionsFrame:IsShown() then
 		Ven.OptionsFrame:Hide()
 	else
+		Ven.OptionsFrame.wasDBOpen = true
+		DBFrame:Hide()
 		Ven.OptionsFrame:Show()
 	end
 end)
@@ -343,6 +346,10 @@ searchBox:SetTextInsets(6, 20, 0, 0)
 searchBox:SetBackdrop({ bgFile = "Interface\\Buttons\\WHITE8x8", edgeFile = "Interface\\Buttons\\WHITE8x8", edgeSize = 1 })
 searchBox:SetBackdropColor(0, 0, 0, 0.5)
 searchBox:SetBackdropBorderColor(0.3, 0.3, 0.3, 0.8)
+
+searchBox:SetScript("OnEscapePressed", function(self)
+	self:ClearFocus()
+end)
 
 searchBox.placeholder = searchBox:CreateFontString(nil, "OVERLAY", "GameFontDisableSmall")
 searchBox.placeholder:SetPoint("LEFT", 6, 0)
@@ -580,8 +587,12 @@ addPlayerBtn:SetPoint("BOTTOMRIGHT", -15, 12)
 Ven.StyleFlatButton(addPlayerBtn)
 addPlayerBtn:SetText("Add Player")
 addPlayerBtn:SetScript("OnClick", function()
+	Ven.SetPopupTitle("Add Player to Database", nil, nil, "", "")
 	Ven.ShowPopup(Ven.Popups["VENDETTA_ADD_PLAYER"])
 end)
+DBFrame:Hide()
+Ven.DBFrame = DBFrame
+tinsert(UISpecialFrames, "VendettaDBFrame")
 
 DBFrame:SetScript("OnShow", function()
 	currentSort = "lastCombat"

Vendetta/Modules/OptionsUI.lua
@@ -14,6 +14,14 @@ f:SetBackdrop({ bgFile = "Interface\\Buttons\\WHITE8x8", edgeFile = "Interface\\
 f:SetBackdropColor(0.05, 0.05, 0.05, 0.92)
 f:SetBackdropBorderColor(0.3, 0.3, 0.3, 0.9)
 f:Hide()
+tinsert(UISpecialFrames, "VendettaOptionsFrame")
+
+f:SetScript("OnHide", function(self)
+	if self.wasDBOpen and Ven.DBFrame then
+		Ven.DBFrame:Show()
+		self.wasDBOpen = false
+	end
+end)
 
 f.title = f:CreateFontString(nil, "OVERLAY", "GameFontHighlightLarge")
 f.title:SetPoint("TOP", 0, -12)
@@ -200,6 +208,10 @@ local function CreateSlider(parent, text, dbKey, yOffset, minVal, maxVal, step, 
 	editBox:SetBackdrop({ bgFile = "Interface\\Buttons\\WHITE8x8", edgeFile = "Interface\\Buttons\\WHITE8x8", edgeSize = 1 })
 	editBox:SetBackdropColor(0.05, 0.05, 0.05, 0.9)
 	editBox:SetBackdropBorderColor(0.4, 0.4, 0.4, 1)
+
+	editBox:SetScript("OnEscapePressed", function(self)
+		self:ClearFocus()
+	end)
 
 	slider:SetScript("OnShow", function(self)
 		local val = Ven.InitHeroDB()[dbKey]
@@ -559,6 +571,7 @@ wlFrame:SetBackdrop({ bgFile = "Interface\\Buttons\\WHITE8x8", edgeFile = "Inter
 wlFrame:SetBackdropColor(0.05, 0.05, 0.05, 0.95)
 wlFrame:SetBackdropBorderColor(0.3, 0.3, 0.3, 0.9)
 wlFrame:Hide()
+tinsert(UISpecialFrames, "Ven_WhitelistFrame")
 
 wlFrame.title = wlFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlightLarge")
 wlFrame.title:SetPoint("TOP", 0, -10)
@@ -578,6 +591,7 @@ local wlNameBox = CreateFrame("EditBox", "VenWlNameBox", wlFrame, "InputBoxTempl
 wlNameBox:SetSize(90, 20)
 wlNameBox:SetPoint("TOPLEFT", 15, -45)
 wlNameBox:SetAutoFocus(false)
+wlNameBox:SetScript("OnEscapePressed", function(self) self:ClearFocus() end)
 local wlNameLbl = wlFrame:CreateFontString(nil, "OVERLAY", "GameFontNormalSmall")
 wlNameLbl:SetPoint("BOTTOMLEFT", wlNameBox, "TOPLEFT", 0, 3)
 wlNameLbl:SetText("Name:")
@@ -586,6 +600,7 @@ local wlNoteBox = CreateFrame("EditBox", "VenWlNoteBox", wlFrame, "InputBoxTempl
 wlNoteBox:SetSize(100, 20)
 wlNoteBox:SetPoint("LEFT", wlNameBox, "RIGHT", 15, 0)
 wlNoteBox:SetAutoFocus(false)
+wlNoteBox:SetScript("OnEscapePressed", function(self) self:ClearFocus() end)
 wlNoteBox:SetMaxLetters(10)
 local wlNoteLbl = wlFrame:CreateFontString(nil, "OVERLAY", "GameFontNormalSmall")
 wlNoteLbl:SetPoint("BOTTOMLEFT", wlNoteBox, "TOPLEFT", 0, 3)
@@ -612,6 +627,7 @@ Ven.Popups["VENDETTA_SET_WHITELIST_NOTE"] = {
 	button1 = "Save",
 	button2 = "Cancel",
 	hasEditBox = true,
+	placeholder = "Enter Note (Max 10 chars)",
 	maxLetters = 10,
 	OnShow = function(self, data)
 		local db = Ven.InitHeroDB()

Vendetta/Modules/Scanner.lua
@@ -404,6 +404,9 @@ combatLogFrame:SetScript("OnEvent", function(self, event, ...)
 				trackTargetsWorld = true,
 				trackWantedsInst = true,
 				trackWantedsWorld = true,
+				whitelistData = true,
+				whitelist = true,
+				whitelistMigrated = true,
 			}
 			for eName, eData in pairs(db) do
 				if type(eData) == "table" and not reservedKeys[eName] then
@@ -484,12 +487,13 @@ CreateFrame("Frame"):SetScript("OnUpdate", function(self, elapsed)
 				end
 
 				if not UnitIsDeadOrGhost(unit) then
-					local isPersWanted = type(db[name]) == "table" and db[name].isWanted
-					local isPersBounty = type(db[name]) == "table" and db[name].isBounty
-					local isNetBounty = db.enableNetwork and Ven.BountyBoard and Ven.BountyBoard[name]
-					local isNetWanted = db.enableNetwork and Ven.WantedBoard and Ven.WantedBoard[name]
-					local isAcctWanted = db.showAccountWide and Ven.AccountWanteds and Ven.AccountWanteds[name]
-					local isAcctBounty = db.showAccountWide and Ven.AccountBounties and Ven.AccountBounties[name]
+					local isWhitelisted = type(db.whitelistData) == "table" and db.whitelistData[name] ~= nil
+					local isPersWanted = not isWhitelisted and type(db[name]) == "table" and db[name].isWanted
+					local isPersBounty = not isWhitelisted and type(db[name]) == "table" and db[name].isBounty
+					local isNetBounty = not isWhitelisted and db.enableNetwork and Ven.BountyBoard and Ven.BountyBoard[name]
+					local isNetWanted = not isWhitelisted and db.enableNetwork and Ven.WantedBoard and Ven.WantedBoard[name]
+					local isAcctWanted = not isWhitelisted and db.showAccountWide and Ven.AccountWanteds and Ven.AccountWanteds[name]
+					local isAcctBounty = not isWhitelisted and db.showAccountWide and Ven.AccountBounties and Ven.AccountBounties[name]
 
 					local playedSpotSound = false
 
@@ -618,7 +622,7 @@ CreateFrame("Frame"):SetScript("OnUpdate", function(self, elapsed)
 						end
 					end
 
-					if db.enableNetwork and not inInstance and UnitCanAttack("player", unit) then
+					if not isWhitelisted and db.enableNetwork and not inInstance and UnitCanAttack("player", unit) then
 						if isNetBounty or isNetWanted then
 							local t = GetTime()
 							local timeSinceLastBountySeen = t - (Ven.bountyLastSeen and Ven.bountyLastSeen[name] or 0)

Vendetta/Modules/TrackerUI.lua
@@ -95,7 +95,8 @@ Ven_EditBox:SetFontObject("ChatFontNormal")
 Ven_EditBox:SetSize(280, 20)
 Ven_EditBox:SetPoint("LEFT", Ven_QuickCopy, "LEFT", 10, 0)
 Ven_EditBox:SetAutoFocus(true)
-Ven_EditBox:SetScript("OnEscapePressed", function()
+Ven_EditBox:SetScript("OnEscapePressed", function(self)
+	self:ClearFocus()
 	Ven_QuickCopy:Hide()
 end)
 Ven_EditBox:SetScript("OnChar", function(self)